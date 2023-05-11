2928440d59990527dad8e501bb1c6e9fbea7590b
Biggbada <yguenot@polepo.fr>
2023-05-11 19:39:37 +0200

    (feat / main : Commit connexion API)
    figma en cours -  fetch ok + optimisation avec Aurelien - scss a l'etude

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
-const wordInput = document.getElementById('word-input');
-const form = document.querySelector('form');
-const rootUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+const wordInput = document.getElementById("word-input");
+const form = document.querySelector("form");
+const rootUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  fetch(rootUrl + wordInput.value)
+    .then((response) => {
+      return response.json();
+    })
+    .then((donnees) => {
+      console.log(donnees[0].word);
+      console.log(donnees[0].meanings);
 
-form.addEventListener('submit', event => {
-        event.preventDefault();
-        var promesse = fetch(rootUrl+ wordInput.value);
-        promesse.then((response) => {
-            console.log(response);
-        var jsonResponse = response.json();
-        console.log(jsonResponse[0]);
-        console.log(jsonResponse.lenght);
-
-        
-        jsonResponse.then (donnees => {
-            console.log(donnees[0].word);
-            console.log(donnees[0].meanings);
-        })
-        for (let i = 0; i < jsonResponse.lenght; i++) {
-            console.log(response[i]);
-
-        }
-        });
-        promesse.then (datas => {
-            console.log(datas[0]);
-        })
-        promesse.catch((errors) => {
-            console.log('erreurs' + errors);
-        })
+      var meanings = donnees[0].meanings;
+      for (let i = 0; i < meanings.length; i++) {
+        console.log(meanings[i]);
+      }
+    });
 });
-
-
-//let's get meaning 1 by one
+//let's get meanings 1 by one
